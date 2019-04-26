ef04d76b6c3831fb64d7bd6fb05a0db24b2ca1cc
kdublam <kdublam@gmail.com>
2019-04-25 21:42:04 -0700

    adding 2 more parallax pics

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -40,3 +40,27 @@ $(document).ready(function(){
 AOS.init({
   duration: 1200,
 });
+
+// $(document).ready(function() {
+//   // grab the initial top offset of the navigation 
+//      var stickyNavTop = $('#navCon').offset().top;
+     
+//      // our function that decides weather the navigation bar should have "fixed" css position or not.
+//      var stickyNav = function(){
+//       var scrollTop = $(window).scrollTop(); // our current vertical position from the top
+           
+//       // if we've scrolled more than the navigation, change its position to fixed to stick to top,
+//       // otherwise change it back to relative
+//       if (scrollTop > stickyNavTop) { 
+//           $('#navCon').addClass('sticky');
+//       } else {
+//           $('#navCon').removeClass('sticky'); 
+//       }
+//   };
+
+//   stickyNav();
+//   // and run it again every time you scroll
+//   $(window).scroll(function() {
+//     stickyNav();
+//   });
+// });
